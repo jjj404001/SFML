af38727713b7b6c45fb9745ddda588506b40e7ec
jooho.jeong <jooho556@gmail.com>
2018-05-05 23:55:24 +0900

    Add EventManager.cpp and .h

---
+++ Chapter 4/EventManager.h
@@ -0,0 +1,79 @@
+#ifndef EVENTMANAGER_H
+#define EVENTMANAGER_H
+
+#include <SFML/Graphics.hpp>
+#include <unordered_map>
+
+enum class EventType
+{
+	KeyDown = sf::Event::KeyPressed,
+	KeyUp = sf::Event::KeyReleased,
+	MButtonDown = sf::Event::MouseButtonPressed,
+	MButtonUp = sf::Event::MouseButtonReleased,
+	MouseWheel = sf::Event::MouseWheelMoved,
+	WindowResized = sf::Event::Resized,
+	GainedFocus = sf::Event::GainedFocus,
+	LostFocus = sf::Event::LostFocus,
+	MouseEntered = sf::Event::MouseEntered,
+	MouseLeft = sf::Event::MouseLeft,
+	Closed = sf::Event::Closed,
+	TextEntered = sf::Event::TextEntered,
+	Keyboard = sf::Event::Count + 1, Mouse, Joystick
+};
+
+struct EventInfo
+{
+	EventInfo() { m_code = 0; }
+	EventInfo(int l_event) { m_code = l_event; }
+	union
+	{
+		int m_code;
+	};
+};
+
+using Events = std::vector<std::pair<EventType, EventInfo>>;
+
+struct EventDetails
+{
+	EventDetails(const std::string & bindName)
+		: m_name(bindName)
+	{
+		Clear();
+	}
+	std::string m_name;
+	sf::Vector2i m_size;
+	sf::Uint32 m_textEntered;
+	sf::Vector2i m_mouse;
+	int m_mouseWheelDelta;
+	int m_keyCode;
+
+	void Clear()
+	{
+		m_size = sf::Vector2i(0, 0);
+		m_textEntered = 0; 
+		m_mouse = sf::Vector2i(0, 0);
+		m_mouseWheelDelta = 0;
+		m_keyCode = -1;
+	}
+};
+
+struct Binding 
+{
+	Binding(const std::string & name)
+		: m_name(name), m_details(name), c(0) {}
+
+	void BindEvent(EventType type, EventInfo info = EventInfo())
+	{
+		m_events.emplace_back(type, info);
+	}
+
+	Events m_events;
+	std::string m_name;
+	int c; // count of events that are "happening"
+
+	EventDetails m_details;
+};
+
+using Bindings = std::unordered_map<std::string, Binding*>;
+
+#endif
