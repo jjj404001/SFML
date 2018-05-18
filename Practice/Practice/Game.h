4215036ddc400ee7b05052cdbf5dd8b76b8cbe54
jooho.jeong <jooho556@gmail.com>
2018-05-18 11:17:31 +0900

    SFML Practice Project

---
+++ Practice/Practice/Game.h
@@ -0,0 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "Window.h"
+
+class Game
+{
+public:
+	void Update();
+	void Draw();
+
+private:
+	
+};
+
+#endif
---
+++ Practice/Practice/Window.h
@@ -0,0 +1,14 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include<SFML/System.hpp>
+
+class Window
+{
+public:
+
+private:
+	sf::Renderwindow 
+};
+
+#endif
